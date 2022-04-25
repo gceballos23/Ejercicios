0c2a58be3fd0d990969731772f3e30ff4e080d7a
gceballos23 <103612856+gceballos23@users.noreply.github.com>
2022-04-25 19:43:32 -0300

    ejercios granjera ejercicio granjerita y ejemplo variable

---
+++ src/20220421/ejercicio05.ts
@@ -0,0 +1,8 @@
+console.log("girar izquierda");
+console.log("girar derecha");
+console.log("avanzar uno");
+console.log("avanzar uno");
+console.log("girar derecha");
+console.log("avanzar uno");
+console.log("si hay montaña quitar");
+console.log("si hay agujero tapar");
---
+++ src/20220425/ejemplo1.ts
@@ -0,0 +1,4 @@
+let numero_1: number = 3;
+let numero_2: number = 1;
+let resultado: number = numero_1 + numero_2;
+console.log(resultado);
