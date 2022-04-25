e499197ba0393708804c026a26dcda39025ee0d0
gceballos23 <103612856+gceballos23@users.noreply.github.com>
2022-04-25 18:58:04 -0300

    Ejercicio 2 pasos preparacion mate

---
+++ src/20220421/ejercicio02.ts
@@ -0,0 +1,8 @@
+console.log("calentar agua");
+console.log("esperar que llegue a 90 grados de temperatura");
+console.log("agarrar mate y bombilla");
+console.log("Poner yerba en el mate");
+console.log("sacudir el mate tapando la boca con la mano");
+console.log("agregar la bombilla en el mate");
+console.log("agregar poco de agua al mate");
+console.log("tomar desde la bombilla");
